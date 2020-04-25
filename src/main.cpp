6e2d83d9a2fc39111bdd1e0a0fd1da81431d4f29
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2020-04-25 23:39:09 +0300

    make interactive

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@
 
 #include <OpenGL/gl.h>
 
+// ----------------------------------------------------------
+// Global Variables
+// ----------------------------------------------------------
+double rotate_y=0;
+double rotate_x=0;
+
 void reshape(int w, int h)
 {
     glViewport(0, 0, w, h);
@@ -23,17 +29,41 @@ void display()
     glClear(GL_COLOR_BUFFER_BIT);
 
     glBegin(GL_QUADS);
-    glColor3f(1.0, 1.0, 1.0);
+    glColor3f(1.0, 1.0 * (rotate_x / 100), 1.0);
     glVertex2i(250, 450);
     glColor3f(0.0, 0.0, 1.0);
     glVertex2i(250, 150);
-    glColor3f(0.0, 1.0, 0.0);
+    glColor3f(0.0, 1.0 * (rotate_y / 100), 0.0);
     glVertex2i(550, 150);
     glColor3f(1.0, 0.0, 0.0);
     glVertex2i(550, 450);
     glEnd();
 
     glutSwapBuffers();
+}
+
+// ----------------------------------------------------------
+// specialKeys() Callback Function
+// ----------------------------------------------------------
+void specialKeys( int key, int x, int y ) {
+
+    //  Right arrow - increase rotation by 5 degree
+    if (key == GLUT_KEY_RIGHT)
+        rotate_y += 5;
+
+        //  Left arrow - decrease rotation by 5 degree
+    else if (key == GLUT_KEY_LEFT)
+        rotate_y -= 5;
+
+    else if (key == GLUT_KEY_UP)
+        rotate_x += 5;
+
+    else if (key == GLUT_KEY_DOWN)
+        rotate_x -= 5;
+
+    //  Request display update
+    glutPostRedisplay();
+
 }
 
 
@@ -48,6 +78,7 @@ int main(int argc, char* argv[]) {
 
     glutReshapeFunc(reshape);
     glutDisplayFunc(display);
+    glutSpecialFunc(specialKeys);
 
     glutMainLoop();
 
